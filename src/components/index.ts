1bc2dbdaab22cfa06aa36c72a01873a55ae22ebf
Nguyễn Văn Phú Nhàn <pnnguyen9999@outlook.com.vn>
2022-04-25 15:02:33 +0700

    init

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,19 @@
-export { Header } from "./header";
-export { Logo } from "./logo";
-export { Main } from "./main";
-export { Button } from "./button";
-export { Cards } from "./cards";
-export { Footer } from "./footer";
+export { ResizableTileMap as TileMap } from "./TileMap/ResizableTileMap";
+// export { Props as TileMapProps } from "./TileMap/TileMap.types";
+// export { Layer, Coord } from "./lib/common";
+export { renderMap } from "./render/map";
+export { renderTile } from "./render/tile";
+export type Tile = {
+  color: string;
+  top?: boolean;
+  left?: boolean;
+  topLeft?: boolean;
+  scale?: number;
+};
+
+export type Coord = {
+  x: number;
+  y: number;
+};
+
+export type Layer = (x: number, y: number) => Tile | null;
